ca35b1b45afeaf539cb222591cf8e4669d091130
Tarak Patel <pateltarak3@gmail.com>
2018-12-30 12:13:27 -0500

    m

--- builder/include/builder.h
+++ builder/include/builder.h
@@ -1,6 +1,8 @@
 #ifndef BUILD_INC
+#ifndef IMG_INC
 #include<iostream>
 #include<opencv2/opencv.hpp>
+#endif
 #define BUID_INC
 #endif
 #ifndef IMG_INC
@@ -10,11 +12,11 @@
 
 class builder{
 public:
-  builder() {this.image.path=NULL;this.image.img=NULL;}
+  builder() {this->image.path=NULL;this->image.img=NULL;}
   builder& loadimage(std::string);
   builder& assignimg(cv::Mat&);
 
 private:
   struct image { std::string path; cv::Mat img;};
 
-}
+};
