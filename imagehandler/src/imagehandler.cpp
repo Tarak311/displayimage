80ef4bb68bfcaeeaa606aa83c8c143ce2eba2619
Tarak Patel <pateltarak3@gmail.com>
2018-12-30 23:27:18 -0500

    wwddww

--- imagehandler/src/imagehandler.cpp
+++ imagehandler/src/imagehandler.cpp
@@ -8,23 +8,13 @@
 #endif
 
 
-
-
-
-
 [[deprecated]]  imagehandler::imagehandler(std::string name){cv::Mat img_temp = cv::imread(name, cv::IMREAD_COLOR);this->img = &img_temp;}
 imagehandler::imagehandler(imagehandler& ih):img(ih.img),path(ih.path){} // ADD VARIABLES PERFORM SHALLOW COPY
 imagehandler::imagehandler(imagehandler&& ih):img(ih.img),path(ih.path){} // move constructor used
-
-int imagehandler::showimage(std::string win,int flag)
-{
-	cv::namedWindow(win, cv::WINDOW_NORMAL);
-	cv::imshow(win, *(this->img));
-	return 0;
-}
-
+int imagehandler::showimage(std::string win,int flag){cv::namedWindow(win, cv::WINDOW_NORMAL);cv::imshow(win, *(this->img));return 0;}
 imagehandler& imagehandler::gethandler(){return *this;}
 
+virtual image& process(){ image imgst = new image; imgst.path = this->path;imgst.img = *(this->img);imgst.output = this->img;}
 // [[deprecated]] imagehandler::imagehandler(cv::Mat& im){this->img=&im;} // PASSED BY REFRENCE - DEPRECATED : REMOVED
 // [[deprecated]] imagehandler::imagehandler(cv::Mat&& im){this->img=&im;} // MOVE SCHEMATICS USED - DEPRECATED : REMOVED
 //imagehandler::imagehandler(const imagehandler& ih) {this->img = ih.img;} //copy constructor -  deleted
