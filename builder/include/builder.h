2ba79904af6f530f906b667d7b23fd1da7c30ffa
Tarak Patel <pateltarak3@gmail.com>
2018-12-31 14:29:38 -0500

    fix

--- builder/include/builder.h
+++ builder/include/builder.h
@@ -11,14 +11,14 @@ class imagehandler;
 #endif
 
 struct cllist {int type ;imagehandler *imghd;};                                                   /* it should have switch statement which selects which type to use*/
-struct image { std::string path; cv::Mat img; image* output; image* input; cllist& (*buildimghd)(int);imagehandler *handler;};/*  code which takes image properties and return imagehandler by calling buildvar = new imagetype::builder.builder().createfromimg().build() */
+struct image { std::string path; cv::Mat img; image* output; image* input; cllist* (*buildimghd)(int);imagehandler *handler;};/*  code which takes image properties and return imagehandler by calling buildvar = new imagetype::builder.builder().createfromimg().build() */
 
 class builder{
 public:
   builder() {}
   builder& loadimage(std::string);
   builder& assignimg(cv::Mat&);
-  cllist createfromimg(image&,int/*type*/); // TODO: New fucntion for creating from image structure. Which should create imagehandler obj from image structure
+  cllist* createfromimg(image&,int/*type*/); // TODO: New fucntion for creating from image structure. Which should create imagehandler obj from image structure
 
 private:
   image imagebuild;
