654492ffc15780daca7de5c27e29033a33ec523a
Tarak Patel <pateltarak3@gmail.com>
2018-12-30 23:13:29 -0500

    ssd

--- builder/include/builder.h
+++ builder/include/builder.h
@@ -8,9 +8,10 @@
 
 #ifndef IMG_INC
 class imagehandler;
-#endif                                                                /* it should have switch statement which selects which type to use*/
-struct image { std::string path; cv::Mat img; image* output; image* input; cllist& (*buildimghd)(int);imagehandler *handler};/*  code which takes image properties and return imagehandler by calling buildvar = new imagetype::builder.builder().createfromimg().build() */
-struct cllist {int type ;imagehandler *imghd };
+#endif
+struct cllist {int type ;imagehandler *imghd;};                                                   /* it should have switch statement which selects which type to use*/
+struct image { std::string path; cv::Mat img; image* output; image* input; cllist& (*buildimghd)(int);imagehandler *handler;};/*  code which takes image properties and return imagehandler by calling buildvar = new imagetype::builder.builder().createfromimg().build() */
+
 class builder{
 public:
   builder() {}
