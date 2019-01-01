ab5b112b99a3e54ea439b528545df2e1717f2969
Tarak Patel <pateltarak3@gmail.com>
2019-01-01 02:22:50 -0500

    wc

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -6,17 +6,20 @@ std::string path;
 
 int main()
 {
-
+	std::cout<<"sts1"<<std::endl;
 	cv::Mat img,dst;
 	builder bb;
+	std::cout<<"sts2"<<std::endl;
 	imagehandler* bbn = bb.loadimage("i.jpg").createfromimg(IMGHDTYPE).imagehandlerd;
+	std::cout<<"sts3"<<std::endl;
 	//bbn->showimage("Display Window", cv::WINDOW_AUTOSIZE);
 	img = cv::imread("i.jpg", cv::IMREAD_COLOR) ;
 	image imga;
 	imga.img=img;
 	imagehandler* ih = new imagehandler(&imga);
-
+  std::cout<<"sts4"<<std::endl;
 	cv::bitwise_xor(img, bbn->imagesaved->img, dst);
+	std::cout<<"sts5"<<std::endl;
 if(cv::countNonZero(dst) > 0) //check non-0 pixels
    std::cout<<"yep"<<std::endl;
 else
