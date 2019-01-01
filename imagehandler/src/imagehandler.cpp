301666b3642c52b3796c983382937eb84a47b23e
Tarak Patel <pateltarak3@gmail.com>
2018-12-31 23:46:19 -0500

    ss

--- imagehandler/src/imagehandler.cpp
+++ imagehandler/src/imagehandler.cpp
@@ -12,7 +12,7 @@
 
 imagehandler::imagehandler(imagehandler& ih):outputimg(ih.outputimg),imagesaved(ih.imagesaved),path(ih.path){}
 imagehandler::imagehandler(imagehandler&& ih):outputimg(ih.outputimg),imagesaved(ih.imagesaved),path(ih.path){}
-imagehandler::imagehandler(cv::Mat& im){this->img=&im;}
-imagehandler::imagehandler(cv::Mat&& im){this->img=&im;}
+imagehandler::imagehandler(image* im){this->imagesaved=im;}
+//imagehandler::imagehandler(image* im){this->img=&im;}
 int imagehandler::showimage(std::string win,int flag){cv::namedWindow(win, cv::WINDOW_NORMAL);cv::imshow(win, *(this->img));return 0;}
 builderlistd imagehandler::process(int TYPE){ if(TYPE==IMGHDTYPE){image im; builderlistd bhdli;im.path = this->path;im.img = *(this->img);im.input = this->imagesaved; builder bu; bu.imagebuild = &im; bu.path=this->path; bhdli->builderd = &bu; return bhdli;}}
