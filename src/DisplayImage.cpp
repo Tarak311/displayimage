90317b64584891aa0e238b71595df73b2d906611
Tarak Patel <pateltarak3@gmail.com>
2019-01-01 01:58:55 -0500

    ccs

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -15,7 +15,7 @@ int main()
 	image imga;
 	imga.img=img;
 	imagehandler* ih = new imagehandler(&imga);
-	if (bbn.imagesaved->img == img){cout<<"whoah<<endl;}
+	if (bbn.imagesaved->img == img){cout<<"whoah"<<endl;}
 	//std::cout<<"Enter the path:"<<std::endl;
 	//std::cin>>path;
 	//imagehandler im(path);
