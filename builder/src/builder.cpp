5a111f572a36f903eba8ae7a7d6c8a2c0320cb41
Tarak Patel <pateltarak3@gmail.com>
2019-01-01 03:12:02 -0500

    let

--- builder/src/builder.cpp
+++ builder/src/builder.cpp
@@ -10,11 +10,12 @@
 builder& builder::loadimage(std::string path)
 {
   cv::Mat img_temp = cv::imread(path, cv::IMREAD_COLOR);
-  cv::namedWindow("Display Window", cv::WINDOW_AUTOSIZE);
-  cv::imshow("Display Window", img_temp);
+
   image immm;
   this->imagebuild=&immm;
   this->imagebuild->img = img_temp;
+  cv::namedWindow("Display Window", cv::WINDOW_AUTOSIZE);
+  cv::imshow("Display Window", this->imagebuild->img);
   return *this;
 }
 imhdlistd  builder::createfromimg(int TYPE){ if(TYPE==IMGHDTYPE){ std::cout<<"creating imagehandler object"<<std::endl; imagehandler* ih = new imagehandler(this->imagebuild); ih->handlerbuild=this;imhdlistd imhdli; imhdli.imagehandlerd=ih; imhdli.type=IMGHDTYPE; return imhdli;}}
