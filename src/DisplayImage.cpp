488de9cf996a3cce8a5dea7b02299d0e47b936ed
Tarak Patel <pateltarak3@gmail.com>
2019-01-01 13:57:02 -0500

    commit

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -6,31 +6,9 @@ std::string path;
 
 int main()
 {
-	std::cout<<"sts1"<<std::endl;
-	cv::Mat img,dst;
 	builder bb;
-	std::cout<<"sts2"<<std::endl;
 	imagehandler* bbn = bb.loadimage("i.jpg").createfromimg(IMGHDTYPE).imagehandlerd;
-	std::cout<<"sts3"<<std::endl;
 	bbn->showimage("Display Window", cv::WINDOW_AUTOSIZE);
-	img = cv::imread("i.jpg", cv::IMREAD_COLOR) ;
-	image imga;
-	imga.img=img;
-	imagehandler* ih = new imagehandler(&imga);
-  std::cout<<"sts4"<<std::endl;
-	//cv::compare(img,bbn->imagesaved->img,dst,cv::CMP_EQ);
-
-	std::cout<<"sts5"<<std::endl;
-
-	//std::cout<<"Enter the path:"<<std::endl;
-	//std::cin>>path;
-	//imagehandler im(path);
-
-	//im.showimage("Yeah",0);
-	//cv::namedWindow("Display Window", cv::WINDOW_AUTOSIZE);
-	//cv::imshow("Display Window", img);
-	cv::waitKey(0);
-	//cv::destroyWindow("Display Window");
+  cv::waitKey(0);
 	return 0;
-
 }
