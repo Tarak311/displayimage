7bdcd47507f251ec9e36bb0fc82c8d37c0c0f680
Tarak Patel <pateltarak3@gmail.com>
2019-01-02 01:26:39 -0500

    wvv

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -9,8 +9,8 @@ int main()
 	builder bb;
 
 	std::shared_ptr<imagehandler>  bbn = bb.loadimage("i.jpg").createfromimg(IMGHDTYPE)->imagehandlerd;
-	bbn->process(IMGHDTYPE);
-	//bbn->showimage("Display Window", cv::WINDOW_AUTOSIZE);
-  //cv::waitKey(0);
+	//bbn->process(IMGHDTYPE);
+	bbn->showimage("Display Window", cv::WINDOW_AUTOSIZE);
+  cv::waitKey(0);
 	return 0;
 }
