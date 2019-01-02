bf4c7a562fc1bd3cb471d57fd54e3466b0da6459
Tarak Patel <pateltarak3@gmail.com>
2019-01-02 12:23:11 -0500

    sjss

--- builder/src/builder.cpp
+++ builder/src/builder.cpp
@@ -13,7 +13,7 @@ std::shared_ptr<builder> builder::loadimage(std::string path)
   std::shared_ptr<image> immm(new image);
   this->imagebuild=immm;
   this->imagebuild->img = img_temp;
-  return this;
+  return shared_from_this();
 }
 
 std::shared_ptr<imhdlistd>  builder::createfromimg(int TYPE)
