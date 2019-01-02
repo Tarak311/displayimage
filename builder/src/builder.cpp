5afdc187197b36b442be91b57a1e347869d6e0a3
Tarak Patel <pateltarak3@gmail.com>
2019-01-02 13:10:06 -0500

    df

--- builder/src/builder.cpp
+++ builder/src/builder.cpp
@@ -12,7 +12,7 @@ std::shared_ptr<builder> builder::loadimage(std::string path)
   std::cout<<"loading image"<<std::endl;
   cv::Mat img_temp = cv::imread(path, cv::IMREAD_COLOR);
   image* iman =new image;
-  std::shared_ptr<image> immm = std::make_shared<image>(new image);
+  std::shared_ptr<image> immm(new image);
     std::cout<<"loaded image"<<std::endl;
   shared_from_this()->imagebuild=immm; //seg fault
   std::cout<<"loaded image2"<<std::endl;
