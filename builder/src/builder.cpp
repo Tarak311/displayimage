de33c0b25832c9bb84f1099a894f963502caa5e6
Tarak Patel <pateltarak3@gmail.com>
2019-01-02 13:28:43 -0500

    dd

--- builder/src/builder.cpp
+++ builder/src/builder.cpp
@@ -11,7 +11,7 @@ std::shared_ptr<builder> builder::loadimage(std::string path)
 {
   std::cout<<"loading image"<<std::endl;
   cv::Mat img_temp = cv::imread(path, cv::IMREAD_COLOR);
-  image  immm = new image;
+  image*  immm = new image;
   std::cout<<"loaded image"<<std::endl;
   (this->imagebuild) = immm ; //seg faul
   std::cout<<"loaded image2"<<std::endl;
