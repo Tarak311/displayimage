c77ffe41320e272f7277d66e444b91f0002b56bd
Tarak Patel <pateltarak3@gmail.com>
2019-01-01 23:08:13 -0500

    dd

--- builder/src/builder.cpp
+++ builder/src/builder.cpp
@@ -8,4 +8,4 @@
 
 
 builder& builder::loadimage(std::string path){cv::Mat img_temp = cv::imread(path, cv::IMREAD_COLOR); image* immm = new image;this->imagebuild=immm;this->imagebuild->img = img_temp;return *this;}
-std::shared_ptr<imhdlistd>  builder::createfromimg(int TYPE){ std::shared_ptr<imhdlistd> imhdli;std::shared_ptr<imagehandler> ih(new imagehandle(this->imagebuild));if(TYPE==IMGHDTYPE){ std::cout<<"creating imagehandler object"<<std::endl;  ih->handlerbuild=this; imhdli->imagehandlerd=ih; imhdli->type=IMGHDTYPE; return imhdli;}}
+std::shared_ptr<imhdlistd>  builder::createfromimg(int TYPE){ std::shared_ptr<imhdlistd> imhdli;std::shared_ptr<imagehandler> ih = new imagehandle(this->imagebuild);if(TYPE==IMGHDTYPE){ std::cout<<"creating imagehandler object"<<std::endl;  ih->handlerbuild=this; imhdli->imagehandlerd=ih; imhdli->type=IMGHDTYPE; return imhdli;}}
