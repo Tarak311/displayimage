7355597b2f54eec01f5dc9bd378ca3db336809e2
Tarak Patel <pateltarak3@gmail.com>
2019-01-02 12:30:50 -0500

    jssjs

--- builder/src/builder.cpp
+++ builder/src/builder.cpp
@@ -12,8 +12,8 @@ std::shared_ptr<builder> builder::loadimage(std::string path)
   std::cout<<"loading image"<<std::endl;
   cv::Mat img_temp = cv::imread(path, cv::IMREAD_COLOR);
   std::shared_ptr<image> immm(new image);
-  std::cout<<"loaded image"<<std::endl;
   this->imagebuild=immm;
+  std::cout<<"loaded image"<<std::endl;
   this->imagebuild->img = img_temp;
 
   return shared_from_this();
