056e36d54fd39d1dc4b50715d56f251b9986f744
Tarak Patel <pateltarak3@gmail.com>
2019-01-03 20:12:12 -0500

    commit

--- builder/src/builder.cpp
+++ builder/src/builder.cpp
@@ -7,5 +7,22 @@
 #endif
 
 
-builder& builder::loadimage(std::string path){cv::Mat img_temp = cv::imread(path, cv::IMREAD_COLOR); image* immm = new image;this->imagebuild=immm;this->imagebuild->img = img_temp;return *this;}
-imhdlistd&  builder::createfromimg(int TYPE){ if(TYPE==IMGHDTYPE){ std::cout<<"creating imagehandler object"<<std::endl; imagehandler* ih = new imagehandler(this->imagebuild); ih->handlerbuild=this;imhdlistd* imhdli = new imhdlistd; imhdli->imagehandlerd=ih; imhdli->type=IMGHDTYPE; return *imhdli;}}
+builder& builder::loadimage(std::string path)
+{
+  cv::Mat img_temp = cv::imread(path, cv::IMREAD_COLOR);
+  image* immm = new image;
+  this->imagebuild=immm;
+  this->imagebuild->img = img_temp;
+  return *this;
+}
+imhdlistd&  builder::createfromimg(int TYPE)
+{
+   if(TYPE==IMGHDTYPE)
+   {
+      std::cout<<"creating imagehandler object"<<std::endl;
+      imagehandler* ih = new imagehandler(this->imagebuild);
+      ih->handlerbuild=this;imhdlistd* imhdli = new imhdlistd;
+      imhdli->imagehandlerd=ih; imhdli->type=IMGHDTYPE;
+      return *imhdli;
+    }
+  }
