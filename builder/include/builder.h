4bc58812c91687519f0d805813e5682e3473d94a
Tarak Patel <pateltarak3@gmail.com>
2019-01-03 19:36:43 -0500

    n

--- builder/include/builder.h
+++ builder/include/builder.h
@@ -28,7 +28,7 @@ class imhdlistd: public listd
     //int get(std::shared_ptr<imagehandler> imd){std::shared_ptr<imagehandler> imagehandlerd1=imd;std::cout<<"setting value "<<std::endl;this->imagehandlerd.reset();this->imagehandlerd =imd;}
 };
 
-class image 
+class image : std::enable_shared_from_this<image>
 {
  public:
   image(){std::cout<<"object const"<<std::endl;}
