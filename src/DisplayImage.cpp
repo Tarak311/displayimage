f45e98884a09273db311eede10f3c62d6e27c4a8
Tarak Patel <pateltarak3@gmail.com>
2019-01-04 07:25:57 +0000

    Update DisplayImage.cpp

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -6,8 +6,8 @@ std::string path;
 
 int main()
 {
-	builder bb
-	std::shared_ptr<builer> b(new builder());
+	builder bb;
+	std::shared_ptr<builder> b(new builder());
 	imagehandler* bbn = bb.loadimage("i.jpg").createfromimg(IMGHDTYPE).imagehandlerd	;
 	bbn->showimage("Display Window", cv::WINDOW_AUTOSIZE);
   cv::waitKey(0);
