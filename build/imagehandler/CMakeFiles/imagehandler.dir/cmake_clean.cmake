04a3610a7da88704f03109d61fd32ec62c2ebc26
Tarak Patel <pateltarak3@gmail.com>
2019-01-09 17:07:27 -0500

    from docker

---
+++ build/imagehandler/CMakeFiles/imagehandler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imagehandler.dir/src/imagehandler.cpp.o"
+  "libimagehandler.pdb"
+  "libimagehandler.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imagehandler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
